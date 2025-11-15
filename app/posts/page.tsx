6966caf38468fd1c06a927a61564a189fecdf712
SuryaSankar <suryashankar.m@gmail.com>
2025-11-15 14:53:15 +0530

    Add links

--- app/posts/page.tsx
+++ app/posts/page.tsx
@@ -15,7 +15,7 @@ export default async function Posts() {
       <ul className="font-[family-name:var(--font-geist-sans)] max-w-2xl space-y-4">
         {posts.map((post) => (
           <li key={post.id}>
-            <span className="font-semibold">{post.title}</span>
+            <span className="font-semibold"><a href={`/posts/${post.id}`}>{post.title}</a></span>
             <span className="text-sm text-gray-600 ml-2">
               by {post.author.name}
             </span>
