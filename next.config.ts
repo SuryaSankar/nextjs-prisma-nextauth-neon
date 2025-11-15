2d4ed613ed1a32644a0504555301d384d9502b56
SuryaSankar <suryashankar.m@gmail.com>
2025-11-15 23:45:35 +0530

    Add experimental

--- next.config.ts
+++ next.config.ts
@@ -2,8 +2,10 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   /* config options here */
-  outputFileTracingIncludes: {
-    '/**/*': ['./app/generated/prisma/**/*'],
+  experimental: {
+    outputFileTracingIncludes: {
+      '/**/*': ['./app/generated/prisma/**/*'],
+    },
   },
 };
 
