c0693065291535ca6cd751d724d165c2520ca1f5
SuryaSankar <suryashankar.m@gmail.com>
2025-11-15 23:35:25 +0530

    Fix next config ts

--- next.config.ts
+++ next.config.ts
@@ -3,7 +3,7 @@ import type { NextConfig } from "next";
 const nextConfig: NextConfig = {
   /* config options here */
   outputFileTracingIncludes: {
-    '/**/*': ['./src/generated/prisma/**/*'],
+    '/**/*': ['./app/generated/prisma/**/*'],
   },
 };
 
