11ab7d3a13f72bd294393e9302ade267c046942c
SuryaSankar <suryashankar.m@gmail.com>
2025-11-15 21:08:36 +0530

    Fix type error

--- app/posts/new/page.tsx
+++ app/posts/new/page.tsx
@@ -10,7 +10,7 @@ export default async function NewPost() {
   async function createPost(formData: FormData) {
     "use server";
 
-    if(!session?.user){
+    if(!session?.user?.id){
       return;
     }
 
